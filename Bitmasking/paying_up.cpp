e3abf0410edc81f246e1ed50e3ef43c38ebf3752
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2020-04-30 01:04:22 +0530

    Create paying_up.cpp

---
+++ Bitmasking/paying_up.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    int n,s;
+    while(t--){
+        cin>>n>>s;
+        int de[n];
+        for(i=0;i<n;i++){
+            cin>>de[i];
+        }
+    }
+
+
+}
