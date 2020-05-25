5e3ff6f4d72268bd11ff3897ef84845357ddbffd
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2020-05-25 20:49:08 +0530

    Update Anagram_Check.py

--- Arrays/Anagram_Check.py
+++ Arrays/Anagram_Check.py
@@ -5,3 +5,32 @@ only the order of characters can be different.
 For example, “abcd” and “dabc” are anagram of each other.
 
 '''
+
+from collections import Counter
+
+def check_anagram(first_string,second_string):
+    d1=Counter(first_string)
+
+    d2= Counter(second_string)
+
+    # print(d1)
+    # print(d2)
+
+    if d1==d2:
+        #print('Are anagrams')
+        return True
+    
+    else:
+        return False
+
+
+
+
+s1= input("Enter first string \n")
+s2= input("Enter second string \n")
+
+if check_anagram(s1,s2):
+    print('True')
+
+else:
+    print('False')
