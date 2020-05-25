28586b3cdc5413d9c759bc3e97f7ba0ed7b80df7
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2020-05-25 20:39:14 +0530

    Create Anagram_Check.py

---
+++ Arrays/Anagram_Check.py
@@ -0,0 +1,7 @@
+'''
+Problem - Given two strings s1 and s2, check if both the strings are anagrams of each other.
+An anagram of a string is another string that contains same characters, 
+only the order of characters can be different. 
+For example, “abcd” and “dabc” are anagram of each other.
+
+'''
