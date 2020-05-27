44fe796660275d7858e8ca814c68418b57759ad6
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2020-05-27 19:12:10 +0530

    Create Array_Pair_Sum.py

---
+++ Arrays/Array_Pair_Sum.py
@@ -0,0 +1,10 @@
+'''
+Given an array of integers, and a number ‘sum’, 
+find the number of pairs of integers in the array whose sum is equal to ‘sum’.
+
+Example- Input  :  arr[] = {1, 5, 7, -1}, 
+          sum = 6
+          Output :  2
+          Pairs with sum 6 are (1, 5) and (7, -1)
+
+'''
