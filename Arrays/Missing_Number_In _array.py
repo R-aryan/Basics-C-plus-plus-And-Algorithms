f354add0f5aefc3a6702afe89292b53b1b858379
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2020-05-27 21:36:51 +0530

    Create Missing_Number_In _array.py

---
+++ Arrays/Missing_Number_In _array.py
@@ -0,0 +1,11 @@
+'''
+Given two arrays which are duplicates of each other(but the elements in the second array is shuffled)
+except one element, that is one element from one of the array is missing,
+we need to find that missing element.
+
+example-Input: arr1[] = {2, 3, 4, 5}
+               arr2[] = {2, 3, 4, 5, 6}
+Output: 6
+6 is missing from first array
+
+'''
