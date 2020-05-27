464393617df05a039d23c9ba504cd04535d6c6e9
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2020-05-27 19:35:01 +0530

    updatd array_pair sum

--- Arrays/Array_Pair_Sum.py
+++ Arrays/Array_Pair_Sum.py
@@ -8,3 +8,31 @@ Example- Input  :  arr[] = {1, 5, 7, -1},
           Pairs with sum 6 are (1, 5) and (7, -1)
 
 '''
+
+def pair_sum(arr,k):
+
+    if len(arr)<=2:
+        return
+    
+    #set for tracking
+    seen=set()
+    Output=set()
+
+    for num in arr:
+        target=k-num
+
+        if target not in seen:
+            seen.add(num)
+        
+        else:
+            Output.add( (min(num,target),max(num,target)))
+    
+    print('\n'.join(map(str,list(Output))))
+
+    return len(Output)
+
+
+#call the function for output
+
+pair= pair_sum([1,7,5,-1],6)
+print(pair)
