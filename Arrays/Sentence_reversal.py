f6069ddfc78077e7a5cd60fe3812c2e6164a72af
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2020-06-01 19:49:17 +0530

    Update Sentence_reversal.py

--- Arrays/Sentence_reversal.py
+++ Arrays/Sentence_reversal.py
@@ -8,3 +8,25 @@ Input- This is the best
 Output- best the is This
 
 '''
+
+def reverse_sentence(sentence):
+    '''
+    sentence- a string of words
+    example- this is best
+    '''
+
+    sentence= sentence.strip()
+
+    l1= sentence.split(' ')
+    #print(l1)
+    s=" "
+
+    for word in l1:
+        s=word+" "+s
+
+    return s.strip()
+
+
+
+res=reverse_sentence("this is the best   ")
+print(res)
