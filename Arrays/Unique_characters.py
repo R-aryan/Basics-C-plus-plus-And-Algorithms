7466b52c60a0aaf88dc03843f8146788eeaad4d5
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2020-06-01 20:34:58 +0530

    Update Unique_characters.py

--- Arrays/Unique_characters.py
+++ Arrays/Unique_characters.py
@@ -6,3 +6,23 @@ Input : s = “abcd”
 Output: True
 “abcd” doesn’t contain any duplicates. Hence the output is True.
 '''
+def check_unique_characters(sen):
+    '''
+    sen = an string example- "aabbdcf"
+    '''
+    chars=set()
+    for i in sen:
+        if i in chars:
+            return False
+        else:
+            chars.add(i)
+    
+    return True
+
+r=check_unique_characters("aabbcdee")
+
+if r:
+    print("All characters are unique")
+
+else:
+    print("Duplicate character present")
