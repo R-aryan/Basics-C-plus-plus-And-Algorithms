14665165da7cd44672dd0d07e40df26a7a868a9f
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2020-06-01 20:01:24 +0530

    Create Unique_characters.py

---
+++ Arrays/Unique_characters.py
@@ -0,0 +1,8 @@
+'''
+To implement an algorithm to determine if a string contains all unique characters.
+
+Examples:
+Input : s = “abcd”
+Output: True
+“abcd” doesn’t contain any duplicates. Hence the output is True.
+'''
