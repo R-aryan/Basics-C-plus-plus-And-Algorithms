fc27e42b76815dd88665430cf728b4a89a5c37cc
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2020-06-01 19:31:55 +0530

    Update Continuous_Sum.py

--- Arrays/Continuous_Sum.py
+++ Arrays/Continuous_Sum.py
@@ -1,3 +1,14 @@
 '''
 Given an array of integers(positive and negative) find the largest continuous sum
 '''
+
+def large_cont_sum(arr):
+
+    if len(arr)==0:
+        return 0
+    
+    max_sum=current_sum=arr[0]
+
+    for num in arr[1:]:
+        current_sum= max(current_sum+num,num)
+        
