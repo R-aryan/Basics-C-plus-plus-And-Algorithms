6fa144ff1ee81efa2a9b733cdcbcc0efd9ffb065
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2020-08-16 20:26:51 +0530

    added implementation for dqueue

--- stacks and Queues/dqueue.py
+++ stacks and Queues/dqueue.py
@@ -6,4 +6,38 @@ class Dqueue(object):
     def isEmpty(self):
         return self.items==[]
     
+    def addFront(self,num):
+        self.items.append(num)
+        print("{} Added at front successfully..!".format(num))
     
+    def addRear(self,num):
+        self.items.insert(0,num)
+        print("{} Added at rear successfully..!".format(num))
+
+    def removeFront(self):
+        r= self.items.pop()
+        print('Successfully popped from front...!!'+str(r))
+    
+    def removeRear(self):
+        r= self.items.pop(0)
+        print("Successfuly removed from rear..!!"+str(r))
+
+    def size(self):
+        l= len(self.items)
+        print("Length of Deque is "+ str(l))
+
+
+d=Dqueue()
+
+d.addFront(12)
+d.addRear(45)
+d.addFront(89)
+d.addFront(78)
+d.addFront(42)
+d.addRear(890)
+d.size()
+
+d.removeFront()
+d.removeRear()
+d.size()
+
